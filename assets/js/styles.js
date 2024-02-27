ec1dd83fb10161932a7bbcb33055344459327b31
jizeanusorn <161416806+jizeanusorn@users.noreply.github.com>
2024-02-27 12:27:15 +0700

    Add files via upload

---
+++ assets/js/styles.js
@@ -0,0 +1,58 @@
+
+// $( document ).ready(function() { // loading excluded all pictures
+//     getABSheight()
+// })
+
+// window.onload = function () { // loading included all pictures
+//     getABSheight()
+//     // alert($( document ).height() - $(".dContent").height())
+// }
+
+// $( document ).ready(function() {
+//     getABSheight()
+// })
+
+// $( window ).resize(function () {
+//     getABSheight()
+// })
+
+// function getABSheight() {
+ 
+//     if ($(document).width() > 768) {
+
+//         let elePadding = $(document).height() - $(".dContent").height()
+        
+//         if ($(".dContent").height()==0)
+//         {
+//             $(".dContent").css("padding-top", $(document).height() / 2)
+//             $(".dContent").css("padding-bottom", $(document).height() / 2)
+//         }
+//         else
+//         {
+//             $(".dContent").css("padding-top", elePadding / 2)
+//             $(".dContent").css("padding-bottom", elePadding / 2)
+//         }
+//     }
+//     else {
+        
+//         if ($(".dContent2").height()==0)
+//         {
+//             let elePadding = $(document).height() - ($(".navbar").height() + $(".footer").height())
+
+//             $(".dContent2").css("padding-top", (elePadding / 2))
+//             $(".dContent2").css("padding-bottom", (elePadding / 2))
+//         }
+//         else
+//         {
+//             $(".dContent2").css("padding-top", "")
+//             $(".dContent2").css("padding-bottom", "")
+//         }
+//     }
+//     // $(".v_parent div:gt(0)").hide()
+//     // $(".v_parent").height($(".v_child").outerHeight())
+// }
+
+// $("#vNode-1").click(function () {
+//     console.log("Your src was changed to path 1")
+//     // $("#your-image").attr("src", "https://www.w3docs.com/uploads/media/default/0001/05/9eb9e9cba721ba3bb5e653751449173197f2924a.png")
+// })
