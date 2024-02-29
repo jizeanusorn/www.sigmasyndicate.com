1d599675876cdaae7915874cc3beed6e259a164e
jizeanusorn <161416806+jizeanusorn@users.noreply.github.com>
2024-02-29 12:47:09 +0700

    Add files via upload

---
+++ assets/js/jquery.flipster.config.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+// $("#carousel").flipster({
+//         style: 'carousel',
+//         spacing: -0.5,
+//         nav: false,
+//         buttons:   false,
+//         loop: true,
+//         //autoplay: 3000,
+//         pauseOnHover: true,
+//    });
+// });
